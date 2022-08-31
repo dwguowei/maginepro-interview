456f0841ecf8eb0d21286715f0d3a879f97b922b
David wong <guowei30@hotmail.com>
2022-08-31 19:52:02 +0800

    Added logo to header

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,36 @@
 import React, {ReactNode} from 'react';
+import styled from "styled-components";
+import LogoSrc from "../../assets/logo.png"
+
+const LayoutContainer = styled.div`
+  display: grid;
+
+  grid-template-areas:
+    'header'
+    'main';
+  gap: 10px;
+  width: 100%;
+`;
+
+const LayoutHeader = styled.header`
+  grid-area: header;
+`;
+
+const LayoutMain = styled.main`
+  grid-area: main;
+`;
+
+const Logo = styled.img`
+    width: 100px;
+`;
 
 const Layout =({children}: {children: ReactNode[] | ReactNode}) =>{
   return(
     <>
-      <div className="layout-container">
-        <header className="layout-header">Header</header>
-        <main className="layout-main">{children}</main>
-      </div>
+      <LayoutContainer>
+        <LayoutHeader className="layout-header"><Logo src={LogoSrc} /></LayoutHeader>
+        <LayoutMain className="layout-main">{children}</LayoutMain>
+      </LayoutContainer>
     </>
   )
 }
